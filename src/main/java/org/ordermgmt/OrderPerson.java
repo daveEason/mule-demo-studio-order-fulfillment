4cc54594c738a5e190bf965e5228530e5a5806db
David Eason <david.eason@mulesoft.com>
2012-04-21 20:44:45 -0700

    Initial development

---
+++ src/main/java/org/ordermgmt/OrderPerson.java
@@ -0,0 +1,97 @@
+package org.ordermgmt;
+
+/**
+ * Person associated with an order.
+ * 
+ * @author Derek
+ */
+public class OrderPerson {
+
+	/** First name */
+	private String firstName;
+
+	/** Last name */
+	private String lastName;
+
+	/** Title */
+	private String title;
+	
+	/** City */
+	private String city;
+
+	/** State */
+	private String state;
+	
+	/** Company Name */
+	private String companyName;
+	
+	/** Phone Number */
+	private String phoneNumber;
+	
+	/** Email */
+	private String email;
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+	
+	public String getTitle() {
+		return title;
+	}
+	
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	
+	public String getCompanyName() {
+		return companyName;
+	}
+	
+	public void setCompanyName(String companyName) {
+		this.companyName = companyName;
+	}
+	
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+	
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+	
+	public String getEmail() {
+		return email;
+	}
+	
+	public void setEmail(String email) {
+		this.email = email;
+	}
+}
